e62b409cd8785031fd509961ca6ce50d33145626
Xiaohan Chi <67724845+xiaohanchi@users.noreply.github.com>
2021-08-15 22:28:31 +0800

    Add files via upload

---
+++ Bayesian Adaptive Designs/Design Calibration/BOBavg/output.R
@@ -0,0 +1,108 @@
+################################################################################
+# BOB: Bayesian Optimal Design for Biosimilar Trials with Co-Primary Endpoints #
+#                                                                              #
+# -Numerical Studies                                                           #
+#  |-Bayesian Adaptive Designs                                                 #
+#    |-Design Calibration                                                      #
+#      |-BOB.avg                                                               #
+################################################################################
+
+###===========================Parameter Searching============================###
+###Grid Searching
+lambda<-seq(0.894,0.901,by=0.0002)
+len1<-length(lambda)
+gamma<-seq(1,1.2,by=0.02)
+len2<-length(gamma)
+for(c in 1:5){
+  totals<-matrix(NA,len1,len2)
+  EN<-matrix(NA,len1,len2)
+  for(m in 1:len1){
+    for(n in 1:len2){
+      nextsn<-sn
+      EN0<-0
+      p4<-1
+      for(t in 1:Tmax){
+        addsample=(maxnsample/Tmax)*t#added up sample size at the current stage
+        Cf<-lambda[m]*(addsample/maxnsample)**gamma[n]
+        if(c==1){
+          deltamu=-0.223
+          mup_bios0<-read.table(paste("deltamu",deltamu,"s",t,".txt",sep = ''))
+        }else if(c==2){
+          deltamu=0.223
+          mup_bios0<-read.table(paste("deltamu",deltamu,"s",t,".txt",sep = ''))
+        }else if(c==3){
+          #power
+          mup_bios0<-read.table(paste("powers",t,".txt",sep = ''))
+        }else if(c==4){
+          deltap=-0.15
+          mup_bios0<-read.table(paste("deltap",deltap,"s",t,".txt",sep = ''))
+        }else if(c==5){
+          deltap=0.15
+          mup_bios0<-read.table(paste("deltap",deltap,"s",t,".txt",sep = ''))
+        }
+        
+        if(t!=1){
+          mup_bios0[addf,1]<- -1
+        }
+        mup_bios<-mup_bios0[,1]
+        stagef<-which(mup_bios < (Cf-0.000001) & mup_bios != -1)
+        mup_bios[stagef]<- -1
+        addf<-which(mup_bios == -1)
+        if(t!=Tmax){
+          EN0<-EN0+(length(stagef)/sn)*addsample
+          p4<- p4-length(stagef)/sn
+        }else if(t==Tmax){
+          EN0<-EN0+p4*addsample
+          EN[m,n]<-EN0
+        }
+        nextstagetrials<-setdiff(c(1:sn),addf)
+        nextsn<-length(nextstagetrials)
+        if(t==Tmax|nextsn==0){
+          totals[m,n]<-nextsn/sn
+        }
+      }
+    }
+  }
+  if(c==1){
+    TIE_el<-totals
+    muEN_l<-EN
+  }else if(c==2){
+    TIE_er<-totals
+    muEN_r<-EN
+  }else if(c==3){
+    EP<-totals
+  }else if(c==4){
+    TIE_sl<-totals
+    pEN_l<-EN
+  }else if(c==5){
+    TIE_sr<-totals
+    pEN_r<-EN
+  }
+}
+
+
+###Calibration
+TIE<-pmax(TIE_el,TIE_er,TIE_sl,TIE_sr)
+EN<-pmax(muEN_l,muEN_r,pEN_l,pEN_r)
+alpha<-which(TIE<=0.05) #alpha_mu
+bEP<-max(EP[alpha])-0.01
+power<-which(EP>=bEP)
+r<-intersect(alpha,power)
+power_n=floor(length(r)/2)
+filter_r<-order(EP[r],decreasing = T)[1:power_n]
+finalset<-r[filter_r]
+value<-finalset[which.min(EN[finalset])]
+
+### Output
+lambda_n<-value%%length(lambda)
+gamma_n<-ceiling(value/length(lambda))
+cat("(",lambda[lambda_n],",",gamma[gamma_n],")",sep = '')
+
+
+
+
+
+
+
+
+
---
+++ Bayesian Adaptive Designs/Design Calibration/BOBavg/search_2.R
@@ -0,0 +1,97 @@
+################################################################################
+# BOB: Bayesian Optimal Design for Biosimilar Trials with Co-Primary Endpoints #
+#                                                                              #
+# -Numerical Studies                                                           #
+#  |-Bayesian Adaptive Designs                                                 #
+#    |-Design Calibration                                                      #
+#      |-BOB.avg                                                               #
+################################################################################
+
+###===========================Simulation Settings============================###
+library(mvtnorm)
+maxnsample=300#max sample size
+Tmax=4 #stages
+nsample=maxnsample/Tmax#sample size per stage
+pT=0.5
+pR=0.5
+overallmuT<- 0
+overallmuR<- 0
+tau2=0.8^2
+rho=0
+pn=20000
+sn=20000
+
+solvemu<-function(p,x,tau2,rho){
+  A=matrix(c(p,(1-p),1,-1),nrow = 2,byrow = T)
+  b=c(x,sqrt(tau2)*rho/sqrt(p*(1-p)))
+  mu<-solve(A,b)
+  return(mu)
+}
+
+muR<-solvemu(pR,overallmuR,tau2,rho)
+muT<-solvemu(pT,overallmuT,tau2,rho)
+sigmaR<-sqrt(tau2-pR*(1-pR)*(muR[1]-muR[2])**2)
+sigmaT<-sqrt(tau2-pT*(1-pT)*(muT[1]-muT[2])**2)
+
+###========================Simulation Implementation=========================###
+mup_bios<-vector(mode="numeric",length=sn)
+for(t in 1:Tmax){
+  addsample=nsample*t#added up sample
+  set.seed(233+10*t)
+  if(t==1){
+    sampleTt<-sapply(1:sn, function(r) rbinom(nsample,1,pT))
+    sampleTe<-sapply(1:sn, function(r) rnorm(nsample,(sampleTt[,r]*muT[1]+(1-sampleTt[,r])*muT[2]),sigmaT))
+    sampleRt<-sapply(1:sn, function(r) rbinom(nsample,1,pR))
+    sampleRe<-sapply(1:sn, function(r) rnorm(nsample,(sampleRt[,r]*muR[1]+(1-sampleRt[,r])*muR[2]),sigmaR))
+  }else{
+    sampleTt2<-sapply(1:sn, function(r) rbinom(nsample,1,pT))
+    sampleTe2<-sapply(1:sn, function(r) rnorm(nsample,(sampleTt2[,r]*muT[1]+(1-sampleTt2[,r])*muT[2]),sigmaT))
+    sampleRt2<-sapply(1:sn, function(r) rbinom(nsample,1,pR))
+    sampleRe2<-sapply(1:sn, function(r) rnorm(nsample,(sampleRt2[,r]*muR[1]+(1-sampleRt2[,r])*muR[2]),sigmaR))
+    sampleTt<-rbind(sampleTt,sampleTt2)
+    sampleTe<-rbind(sampleTe,sampleTe2)
+    sampleRt<-rbind(sampleRt,sampleRt2)
+    sampleRe<-rbind(sampleRe,sampleRe2)
+  }
+  sumyT<-colSums(sampleTt)
+  sumyR<-colSums(sampleRt)
+  pThat<-sumyT/addsample
+  pRhat<-sumyR/addsample
+  mean_deltap<-(sumyT-sumyR)/(addsample+2)
+  var_deltap<-((sumyT+1)*(addsample-sumyT+1)+(sumyR+1)*(addsample-sumyR+1))/((addsample+2)^2*(addsample+3))
+  #mu
+  xbarT<-sapply(1:sn,function(r) mean(sampleTe[,r]))
+  varT<-sapply(1:sn,function(r) var(sampleTe[,r]))
+  xbarR<-sapply(1:sn,function(r) mean(sampleRe[,r]))
+  varR<-sapply(1:sn,function(r) var(sampleRe[,r]))
+  mean_deltamu<-xbarT-xbarR
+  var_deltamu<-(addsample-1)/(addsample^2)*(varT+varR)
+  
+  #mu1T
+  datax1T <- lapply(1:sn,function(r) sampleTe[which(sampleTt[,r]==1),r])
+  xbar1T<-sapply(1:sn,function(r) mean(datax1T[[r]]))
+  #mu2T
+  datax2T <- lapply(1:sn,function(r) sampleTe[which(sampleTt[,r]==0),r])
+  xbar2T<-sapply(1:sn,function(r) mean(datax2T[[r]]))
+  #mu1R
+  datax1R <- lapply(1:sn,function(r) sampleRe[which(sampleRt[,r]==1),r])
+  xbar1R<-sapply(1:sn,function(r) mean(datax1R[[r]]))
+  #mu2R
+  datax2R <- lapply(1:sn,function(r) sampleRe[which(sampleRt[,r]==0),r])
+  xbar2R<-sapply(1:sn,function(r) mean(datax2R[[r]]))
+  rho_deltahat<-(pThat*(1-pThat)*(xbar1T-xbar2T)+pRhat*(1-pRhat)*(xbar1R-xbar2R))/sqrt((pThat*(1-pThat)+pRhat*(1-pRhat))*(varT+varR))
+  for(i in 1:sn){
+    Sig_joint<-matrix(c(var_deltap[i],rho_deltahat[i]*sqrt(var_deltap[i]*var_deltamu[i]),
+                        rho_deltahat[i]*sqrt(var_deltap[i]*var_deltamu[i]),var_deltamu[i]),
+                      nrow=2,ncol=2,byrow=T)
+    mup_bios[i]<-pmvnorm(lower=c(-0.15,-0.223), upper=c(0.15,0.223), 
+                         mean=c(mean_deltap[i],mean_deltamu[i]), 
+                         sigma=Sig_joint, alg=Miwa())[1]
+  }
+  write.table(mup_bios,file = paste("powers",t,".txt",sep = ''))#Save results
+}
+
+
+
+
+  
