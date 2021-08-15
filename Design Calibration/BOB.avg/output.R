f4e6c0d34c2417fd6a3d6fccf53018b28fffb1d5
Xiaohan Chi <67724845+xiaohanchi@users.noreply.github.com>
2021-08-15 22:20:37 +0800

    Add files via upload

--- Design Calibration/BOB.avg/output.R
+++ Design Calibration/BOB.avg/output.R
@@ -2,8 +2,9 @@
 # BOB: Bayesian Optimal Design for Biosimilar Trials with Co-Primary Endpoints #
 #                                                                              #
 # -Numerical Studies                                                           #
-#  --Design Calibration                                                        #
-#    ---BOB.avg                                                                #
+#  |-Bayesian Adaptive Designs                                                 #
+#    |-Design Calibration                                                      #
+#      |-BOB.avg                                                               #
 ################################################################################
 
 ###===========================Parameter Searching============================###
